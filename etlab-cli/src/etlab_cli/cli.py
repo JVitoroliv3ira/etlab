eb8fb0beb0cc5267902a4f59b642e54e2962bbae
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-08-30 19:59:05 -0300

    feat: Adiciona parametro path e utiliza funcao de ler pipeline

--- etlab-cli/src/etlab_cli/cli.py
+++ etlab-cli/src/etlab_cli/cli.py
@@ -1,4 +1,7 @@
+from pathlib import Path
 import typer
+
+from etlab_cli.pipelines.parser import parse_pipeline
 
 app = typer.Typer()
 
@@ -7,8 +10,14 @@ def version() -> None:
     print('Version command')
 
 @app.command(help='Executa pipeline definida no arquivo yaml')
-def run() -> None:
-    print('Run command')
+def run(path: Path) -> None:
+    result = parse_pipeline(path)
+    if result.is_ok():
+        pipeline = result.ok
+        print(pipeline.name)
+    else:
+        err = result.error
+        print(err)
 
 if __name__ == '__main__':
     app()
